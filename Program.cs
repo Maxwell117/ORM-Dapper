a8ae1b7723be3d336781b29fd791ab4939fa5049
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-21 22:10:33 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using ORM_Dapper;
+using ORM_Dapper;
 using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 using System.Data;
@@ -23,6 +23,35 @@ foreach(var department in departments)
 {
     Console.WriteLine(department.DepartmentID);
     Console.WriteLine(department.Name);
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+var productRepo = new DapperProductRepository(conn);
+var productToUpdate = productRepo.GetProduct(974);
+productToUpdate.Name = "UPDATED!";
+productToUpdate.OnSale = true;
+productToUpdate.Price = 12.99;
+productToUpdate.StockLevel = 1000;
+productToUpdate.CategoryID = 1;
+
+productRepo.UpdateProduct(productToUpdate);
+
+productRepo.DeleteProduct(974);
+
+
+
+
+var products = productRepo.GetAllProducts();
+
+foreach (var product in products)
+{
+    Console.WriteLine(product.ProductID);
+    Console.WriteLine(product.Name);
+    Console.WriteLine(product.Price);
+    Console.WriteLine(product.CategoryID);
+    Console.WriteLine(product.OnSale);
+    Console.WriteLine(product.StockLevel);
     Console.WriteLine();
     Console.WriteLine();
 }
